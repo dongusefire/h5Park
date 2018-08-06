53ea545b2fd4d2b1db5451a296d71bba943ed708
guojie <guojie@ecosysnet.com>
2018-08-06 10:25:58 +0800

    guojie

--- js/app.js
+++ js/app.js
@@ -16,6 +16,18 @@ var app = {
 		}else{
 			location.href = str;
 		};
+	},
+	getRequest:function() {    
+	   var url = location.search; //获取url中"?"符后的字串    
+	   var theRequest = new Object();    
+	   if (url.indexOf("?") != -1) {        
+	       var str = url.substr(1);        
+	       strs = str.split("&");        
+	       for(var i = 0; i < strs.length; i ++) {          
+	           theRequest[strs[i].split("=")[0]] = decodeURI(strs[i].split("=")[1]);
+	       }
+	   }
+	   return theRequest;
 	}
 };
 //发送验证码  resCallback:ajax成功后的回调；completeCallback:ajax完成后的回调
